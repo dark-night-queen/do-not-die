ebe8a6250b52522bf4f7f113995e695486e0c8d5
Shreya <agrawal.shreya@icloud.com>
2025-05-22 23:34:05 +0530

    added screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,9 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React from "react";
+import { Tabs } from "expo-router";
+import { Platform } from "react-native";
+import { BarChart2, Home, User } from "lucide-react-native";
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
@@ -14,16 +14,31 @@ export default function TabLayout() {
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          // tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color }) => <Home size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="analytics"
+        options={{
+          title: "Analytics",
+          tabBarIcon: ({ color }) => <BarChart2 size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <User size={28} color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,2 @@
-import { StyleSheet, View , Text} from 'react-native';
-
-export default function HomeScreen() {
-  return (
-    <View><Text>
-      hi</Text></View>
-  );
-}
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+import Home from "@/screens/home";
+export default () => <Home />;
