6c2532154ec8091fe74d90b0bcf2615c1681daf6
Shreya <agrawal.shreya@icloud.com>
2025-07-24 02:05:59 +0530

    added gemini api

--- src/apis/gemini.ts
+++ src/apis/gemini.ts
@@ -1,4 +1,4 @@
-import { getGeminiModel } from "@/lib/models/gemini";
+import { getGeminiModel } from "@/src/lib/genai/models/gemini";
 
 const apiKey = process.env.EXPO_PUBLIC_GOOGLE_API_KEY; // Or use a safer method for RN/Expo
 
