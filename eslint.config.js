4796e8a7a76cc8b8846690b6cf0b4b5f4fdfe204
Shreya <agrawal.shreya@icloud.com>
2026-02-08 22:23:25 +0530

    added husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const expo = require("eslint-config-expo/flat");
+const prettier = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
-  expoConfig,
-  eslintPluginPrettierRecommended,
+  ...expo,
+  prettier,
   {
-    ignores: ["dist/*"],
+    ignores: ["dist/*", ".expo/*", "node_modules/*"],
+  },
+  {
+    rules: {
+      "react/display-name": "off",
+    },
   },
 ]);
